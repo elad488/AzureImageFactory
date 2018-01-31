37d499097ec12ee09a8a43d48d3ce782049e38d3
Elad Hascal <elhascal@microsoft.com>
2018-01-31 16:27:01 +0200

    sc

--- deprovision.sh
+++ deprovision.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-useradd imageuser
-sudo echo Elha47480611 | passwd imageuser --stdin
-
-echo "imageuser ALL = (ALL) NOPASSWD: ALL" >> /etc/sudoers.d/waagent
+echo "vmadmin ALL = (ALL) NOPASSWD: ALL" >> /etc/sudoers.d/waagent
 
 
